40a1e099c806e64867b05e3902e29e546b00df7c
Alejandro Garcia <jandro_es@hotmail.com>
2024-02-19 19:58:42 +0100

    Multiples cambios, cambiamos varios estilos, añadimos el modo oscuro y vamos arreglando la responsividad

---
+++ scripts/darkMode.js
@@ -0,0 +1,4 @@
+// JavaScript para el botón de modo oscuro
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+  });
